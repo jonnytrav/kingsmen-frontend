94e27853e92433d70f26f46c80843190b648d6f0
Jonathan Traverso <jtraverso7@gmail.com>
2021-03-02 19:05:42 -0500

    Implemented iTunes search functionality.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,48 @@
+import { useState } from "react";
+import Results from "../Results";
+
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [terms, setSearchTerms] = useState("");
+  const search = () => {
+    const cleanedTerms = terms.replace(" ", "+");
+    const url = new URL("https://itunes.apple.com/search");
+    const routeParams = { term: cleanedTerms, media: "musicVideo" };
+    url.search = new URLSearchParams(routeParams);
+    fetch(url, { method: "GET" })
+      .then(results => results.json())
+      .then(data => {
+        console.log(data.results);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div className="app-container">
+      <div className="search-container">
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            search();
+          }}
+        >
+          <input
+            autoFocus
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck="false"
+            id="search"
+            onChange={e => {
+              setSearchTerms(e.target.value);
+              //   console.log(params);
+            }}
+          ></input>
+        </form>
+      </div>
+      <Results />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Results/index.js
@@ -0,0 +1,5 @@
+const Results = props => {
+  return <div className="results-div">Smoke Test ^_^</div>;
+};
+
+export default Results;
